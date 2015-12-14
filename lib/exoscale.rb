f779dec4e8ad947e61eb2553225748095c8b3e3b
Nicolas Brechet <nicolasbrechet@me.com>
2015-12-14 10:21:03 +0100

    DNS API

--- lib/exoscale.rb
+++ lib/exoscale.rb
@@ -1,8 +1,9 @@
 require "exoscale/version"
 require "exoscale/compute"
+require "exoscale/dns"
 
 module Exoscale
   COMPUTE_ENDPOINT = "https://api.exoscale.ch:443/compute"
-  DNS_ENDPOINT = "https://api.exoscale.ch/dns"
+  DNS_ENDPOINT = "https://api.exoscale.ch/dns/v1"
   APP_ENDPOINT = "https://api.app.exo.io:443"
 end
